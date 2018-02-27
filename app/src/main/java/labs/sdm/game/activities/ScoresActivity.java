feda174620ab436d296c27036807e3a7be11ea27
VictorIranzo <viranzo96@gmail.com>
2018-02-27 23:49:47 +0100

    Changed Scores Activity.

--- app/src/main/java/labs/sdm/game/activities/ScoresActivity.java
+++ app/src/main/java/labs/sdm/game/activities/ScoresActivity.java
@@ -1,20 +1,9 @@
 package labs.sdm.game.activities;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 import labs.sdm.game.R;
 
 public class ScoresActivity extends AppCompatActivity {
@@ -22,6 +11,13 @@ public class ScoresActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favourite);
+        setContentView(R.layout.activity_scores);
+        TabHost host = (TabHost) findViewById(R.id.tabHost_Scores);
+        host.setup();
+
+        TabSpec spec = host.newTabSpec(getResources().getString(R.string.tab_friends));
+        spec.setIndicator(getResources().getString(R.string.tab_friends));
+        spec.setContent(R.id.la);
+        host.addTab(spec);
     }
 }
