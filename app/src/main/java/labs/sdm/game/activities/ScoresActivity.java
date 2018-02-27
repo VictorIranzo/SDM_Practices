6ecc47444545d19634e9aef44496bebac28d3d4c
VictorIranzo <viranzo96@gmail.com>
2018-02-28 00:43:54 +0100

    Added tabs and list view implementations for Scores Activity using SimpleListView.

--- app/src/main/java/labs/sdm/game/activities/ScoresActivity.java
+++ app/src/main/java/labs/sdm/game/activities/ScoresActivity.java
@@ -2,11 +2,20 @@ package labs.sdm.game.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Adapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import labs.sdm.game.R;
 
 public class ScoresActivity extends AppCompatActivity {
+
+    public ArrayList<HashMap<String,String>> scores = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +24,43 @@ public class ScoresActivity extends AppCompatActivity {
         TabHost host = (TabHost) findViewById(R.id.tabHost_Scores);
         host.setup();
 
-        TabSpec spec = host.newTabSpec(getResources().getString(R.string.tab_friends));
-        spec.setIndicator(getResources().getString(R.string.tab_friends));
-        spec.setContent(R.id.la);
-        host.addTab(spec);
+        TabSpec spec1 = host.newTabSpec(getResources().getString(R.string.tab_local));
+        spec1.setIndicator(getResources().getString(R.string.tab_local));
+        spec1.setContent(R.id.tab1);
+        host.addTab(spec1);
+
+        TabSpec spec2 = host.newTabSpec(getResources().getString(R.string.tab_friends));
+        spec2.setIndicator(getResources().getString(R.string.tab_friends));
+        spec2.setContent(R.id.tab2);
+        host.addTab(spec2);
+
+        host.setCurrentTab(0);
+
+        ListView localTableScores = findViewById(R.id.list1);
+
+        this.getFriendsScores();
+        this.getLocalScores();
+
+        SimpleAdapter adapter = new SimpleAdapter(this, scores, android.R.layout.simple_list_item_2,
+                new String[]{"player","score"}, new int[]{android.R.id.text1, android.R.id.text2});
+
+        localTableScores.setAdapter(adapter);
     }
+
+    private void addScore(String name, int points){
+        HashMap<String,String> item = new HashMap<String,String>();
+        item.put("player", name);
+        item.put("score", String.valueOf(points));
+        scores.add(item);
+    }
+
+    private void getFriendsScores() {
+        addScore("Paco",20);
+        addScore("Vicente",1000);
+    }
+
+    private void getLocalScores() {
+
+    }
+
 }
