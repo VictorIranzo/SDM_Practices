c6490a41b12620a44965b08cc20b52d8e4e4b08c
VictorIranzo <viranzo96@gmail.com>
2018-02-28 02:04:18 +0100

    Designed Play view.

--- app/src/main/java/labs/sdm/game/activities/PlayActivity.java
+++ app/src/main/java/labs/sdm/game/activities/PlayActivity.java
@@ -10,6 +10,6 @@ public class PlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quotation);
+        setContentView(R.layout.activity_play);
     }
 }
